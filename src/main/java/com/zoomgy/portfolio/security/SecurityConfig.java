5e42d740895136a2ed977f92d52042a2cea1a10e
Singh <ayush-pratap.b.singh@capgemini.com>
2026-05-08 11:56:09 +0530

    Updated the frontend URL to allow CORS policy

--- src/main/java/com/zoomgy/portfolio/security/SecurityConfig.java
+++ src/main/java/com/zoomgy/portfolio/security/SecurityConfig.java
@@ -26,7 +26,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://portfolio-frontend-six-ruby.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
